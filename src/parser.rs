bd3733032ecfbf79fcbe8a91ec1a835db31b9afa
tom <t.vitureau@purjus.fr>
2019-07-27 15:11:24 +0200

    clean architectur

---
+++ src/parser.rs
@@ -0,0 +1,34 @@
+use linked_hash_map::LinkedHashMap;
+use std::fs::File;
+use std::io::Read;
+use yaml_rust::{Yaml, YamlLoader};
+
+pub fn get_doc() -> LinkedHashMap<Yaml, Yaml> {
+    let mut f =
+        File::open("fixtures/sample.yaml").expect("Error while opening fixtures/sample.yaml");
+    let mut s = String::new();
+    f.read_to_string(&mut s)
+        .expect("Cannot read fixtures/sample.yaml");
+
+    let docs = YamlLoader::load_from_str(&s).expect("Cannot parse fixtures/sample.yaml");
+    let doc = docs[0].clone().into_hash().unwrap();
+
+    doc
+}
+
+pub fn yaml_element_as_string(value: &Yaml) -> String {
+    let expression: String;
+    if value.as_str().is_some() {
+        expression = value.clone().into_string().unwrap();
+    } else if value.as_i64().is_some() {
+        expression = value.clone().into_i64().unwrap().to_string();
+    } else if value.as_f64().is_some() {
+        expression = value.clone().into_f64().unwrap().to_string();
+    } else if value.as_bool().is_some() {
+        expression = value.clone().into_bool().unwrap().to_string();
+    } else {
+        unimplemented!();
+    }
+
+    expression
+}

87675bc34c99bd6cc3631485d58952898adecbd3
tom <t.vitureau@purjus.fr>
2019-07-29 00:08:30 +0200

    add tests

---
+++ bin/test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -eu
+
+# style
+cargo fmt --all -- --check
+
+# tasks
+get_task() {
+    grep -E "^[a-z0-9_-.]+:" rumake.yaml | cut -d ":" -f 1 | grep -v -E ".*$1.*"
+}
+
+EXCLUDE="fail"
+TASKS=$(get_task $EXCLUDE)
+
+for task in $TASKS
+do
+    cargo run $task
+done
+
+EXCLUDE="ls|cargo|ps|fail"
+TASKS=$(get_task $EXCLUDE)
+
+for task in $TASKS
+do
+    cargo run $task foo
+done
+
+# failure is what we want
+set +e
+cargo run fail
+if [[ "$?" != 101 ]]; then exit 1; fi

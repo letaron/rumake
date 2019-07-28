735e7774b2f87e138396f5213738af1ada18b7bc
tom <t.vitureau@purjus.fr>
2019-07-28 15:22:26 +0200

    better argument parsing

--- src/runner.rs
+++ src/runner.rs
@@ -1,6 +1,8 @@
 use crate::Task;
 use log::{debug, info};
+use shellwords;
 use std::collections::HashMap;
+use std::mem;
 use std::process::{Command, Stdio};
 
 pub fn exec_task(
@@ -26,6 +28,7 @@ pub fn exec_task(
             let mut new_command_call_stack = command_call_stack.clone();
             new_command_call_stack.push(&task_name);
 
+            // remove the first char "@"
             let referenced_task_name = instruction.to_string().split_off(1);
             debug!("  -> run {}", referenced_task_name);
             exec_task(
@@ -38,20 +41,22 @@ pub fn exec_task(
             continue;
         }
 
-        run_instruction(task, instruction, &call_args, variables);
+        run_instruction(instruction, &call_args, variables);
     }
 }
 
 fn run_instruction(
-    task: &Task,
     instruction: &String,
     call_args: &Vec<String>,
     variables: &HashMap<String, String>,
 ) {
-    let mut command = Command::new("sh");
-    let instruction = expand_instruction(task, instruction, call_args, variables);
+    let instructions_parts = shellwords::split(&instruction).unwrap();
+    let (program, program_args) = instructions_parts.split_at(1);
+    let program_args = expand_program_args(program_args.to_vec(), call_args, variables);
+    let program = &program[0];
+    let mut command = Command::new(program);
 
-    command.args(vec!["-e", "-u", "-c"]).arg(&instruction);
+    command.args(&program_args);
 
     info!("{:?}", command);
 
@@ -60,35 +65,50 @@ fn run_instruction(
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .output()
-        .expect(&format!("Command '{}' failed.", instruction));
+        .expect(&format!("Command '{:?}' failed.", program_args));
 
     if !output.status.success() {
         match output.status.code() {
-            Some(code) => panic!("Command '{}' failed with code {}.", instruction, code),
-            None => panic!("Command '{}' terminated by signal", instruction),
+            Some(code) => panic!("Command '{:?}' failed with code {}.", program_args, code),
+            None => panic!("Command '{:?}' terminated by signal", program_args),
         }
     }
 }
 
-fn expand_instruction(
-    task: &Task,
-    instruction: &String,
+fn expand_program_args(
+    program_args: Vec<String>,
     call_args: &Vec<String>,
     variables: &HashMap<String, String>,
-) -> String {
-    let mut instruction = instruction.to_string();
+) -> Vec<String> {
+    debug!("program_args: {:?}", program_args);
 
-    debug!("  original: {}", instruction);
-    for (name, value) in variables {
-        instruction = instruction.replace(name, value);
-    }
-    debug!("  replaced: {}", instruction);
+    let mut processed_args = program_args.clone();
 
-    if task.instructions.len() == 1 {
-        for call_arg in call_args {
-            instruction = format!("{} {}", instruction, call_arg);
+    for (index, program_arg) in program_args.iter().enumerate() {
+        if !variables.contains_key(program_arg) {
+            debug!("  parameter unknown {}", program_arg);
+            continue;
         }
+
+        let value = variables.get(program_arg).unwrap();
+
+        mem::replace(&mut processed_args[index], value.to_string());
+        debug!("  replaced {} by {}", program_arg, value);
+    }
+    debug!("processed_args: {:?}", processed_args);
+
+    if let Some(index) = processed_args.iter().position(|x| x == "$RUMAKE_ARGS") {
+        let (left, right) = processed_args.split_at(index);
+        let mut right = right.to_vec();
+        right.remove(0);
+
+        processed_args = left.to_vec();
+        processed_args.extend_from_slice(&call_args);
+        processed_args.extend_from_slice(&right);
+
+        // panic!("-- {:?}  {:?} -- {:?} {:?}", processed_args,  index, left, right);
+        debug!("  rumake args remplaced: {:?}", call_args);
     }
 
-    instruction
+    processed_args
 }
